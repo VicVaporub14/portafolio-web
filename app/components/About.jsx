5a35e57108dc09f82a70d9428659b7e6a086c540
VicVaporub14 <212310861@itslerdo.edu.mx>
2025-04-13 23:48:07 -0600

    Agregacion de animacion en seccion About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,26 +1,69 @@
 import { assets, infoList, toolsData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from 'motion/react'
 
 export default function About({isDarkMode}) {
   return (
-    <div id='about' className='w-full py-10 px-6 scroll-mt-20 mx-auto'> {/* px-[12%] */}
-        <h4 className='text-center mb-2 text-lg font-ovo'>Introduction</h4>
-        <h2 className='text-center text-4xl font-ovo'>About me</h2>
-        <div className='flex w-full flex-col lg:flex-row lg:justify-center items-center gap-10 my-10 mx-auto'>
-            <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
+    <motion.div 
+        initial={{opacity: 0}}
+        whileInView={{opacity:1}}
+        transition={{duration: 1.5}}
+        id='about' 
+        className='w-full py-10 px-6 scroll-mt-20 mx-auto'
+    > {/* px-[12%] */}
+        <motion.h4 
+            initial={{opacity: 0, y: -20}}
+            whileInView={{opacity:1, y: 0}}
+            transition={{duration: 0.5, delay: 0.5}}
+            className='text-center mb-2 text-lg font-ovo'
+        >Introduction</motion.h4>
+
+        <motion.h2 
+            initial={{opacity: 0, y: -20}}
+            whileInView={{opacity:1, y: 0}}
+            transition={{duration: 0.5, delay: 0.5}}
+            className='text-center text-4xl font-ovo'
+        >About me</motion.h2>
+
+        <motion.div 
+            initial={{opacity: 0}}
+            whileInView={{opacity:1}}
+            transition={{duration: 0.8}}
+            className='flex w-full flex-col lg:flex-row lg:justify-center items-center gap-10 my-10 mx-auto'
+        >
+            <motion.div 
+                initial={{opacity: 0, scale: 0.9}}
+                whileInView={{opacity:1, scale: 1}}
+                transition={{duration: 0.6}}
+                className='w-64 sm:w-80 rounded-3xl max-w-none'
+            >
                 <Image 
                     src={assets.user_image} 
                     alt='user' 
                     className='w-full rounded-3xl'
                 />
-            </div>
-            <div className='flex flex-col items-center'> {/* flex-1 */}
+            </motion.div>
+            <motion.div 
+                initial={{opacity: 0}}
+                whileInView={{opacity:1}}
+                transition={{duration: 0.6, delay: 0.8}}
+                className='flex flex-col items-center'
+            > {/* flex-1 */}
                 <p className='mb-10 text-center max-w-2xl font-ovo '>I'm a tech-loving student, who enjoys both frontend and backend development. My focus is on web development and database management, always striving to learn and sharpening my skills. I've worked on projects where I've applied technologies such as React, Next.js, Node.js, .NET, PostgreSQL, and MongoDB.</p>
 
-                <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
+                <motion.ul 
+                    initial={{opacity: 0}}
+                    whileInView={{opacity:1}}
+                    transition={{duration: 0.8, delay: 1}}
+                    className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'
+                >
                     {infoList.map(({icon, iconDark, title, description}, index) => (
-                        <li className='text-center border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-blue-50 hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-customBlue' key={index}>
+                        <motion.li 
+                            whileHover={{scale: 1.05}}
+                            className='text-center border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-blue-50 hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-customBlue' 
+                            key={index}
+                        >
                             <Image 
                                 src={isDarkMode ? iconDark : icon} 
                                 alt={title} 
@@ -28,23 +71,35 @@ export default function About({isDarkMode}) {
                             />
                             <h3 className='my-4 font-semibold text-gray-700 dark:text-white'>{title}</h3>
                             <p className='text-gray-600 text-sm dark:text-white/80'>{description}</p>
-                        </li>
+                        </motion.li>
                     ))}
-                </ul>
+                </motion.ul>
 
-                <h4 className='my-6 text-gray-700 font-ovo dark:text-white'>Tools I use</h4>
-                <ul className='w-72 sm:w-full grid grid-cols-5 sm:flex items-center sm:justify-center gap-3 sm:gap-5 rounded-xl p-2'>
+                <motion.h4 
+                    initial={{opacity: 0, y: 20}}
+                    whileInView={{opacity:1, y: 0}}
+                    transition={{duration: 1.3, delay: 0.5}}
+                    className='my-6 text-gray-700 font-ovo dark:text-white'
+                >Tools I use</motion.h4>
+
+                <motion.ul 
+                    initial={{opacity: 0}}
+                    whileInView={{opacity:1}}
+                    transition={{duration: 1.5, delay: 0.6}}
+                    className='w-72 sm:w-full grid grid-cols-5 sm:flex items-center sm:justify-center gap-3 sm:gap-5 rounded-xl p-2'
+                >
                     {toolsData.map((tool, index) => (
-                        <li 
+                        <motion.li 
+                        whileHover={{scale: 1.1}}
                             key={index}
                             className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500 dark:bg-slate-600'
                         >
                             <Image src={tool} alt='Tool' className='w-5 sm:w-7 rounded-lg' />
-                        </li>
+                        </motion.li>
                     ))}
-                </ul>
-            </div>
-        </div>
-    </div>
+                </motion.ul>
+            </motion.div>
+        </motion.div>
+    </motion.div>
   )
 }
